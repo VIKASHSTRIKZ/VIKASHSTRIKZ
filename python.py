cc5880b83de86144d82e4d2b65d27d275103a831
STRIKZ <112474234+VIKASHSTRIKZ@users.noreply.github.com>
2022-09-01 14:25:15 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+list1=[1,'22AIOO1',9]
+list1
+[1, '22AIOO1', 9]
+type(list1)
+<class 'list'>
+list2=[1.0,2.0,3.0]
+list2
+[1.0, 2.0, 3.0]
+type(list2)
+<class 'list'>
+list1==list2
+False
+t1=(1,2,3)
+t2=(2,1,3)
+t3=(t1,t2)
+print(t3)
+((1, 2, 3), (2, 1, 3))
+print(t1+t2)
+(1, 2, 3, 2, 1, 3)
+print(t1*2)
+(1, 2, 3, 1, 2, 3)
